ba4100bdff07676bb8fe137ef68ef50a7610249c
erwinagpasa <rootaccess000@gmail.com>
2024-01-07 11:34:31 +0300

    Initial Commit

---
+++ PeopleMVVM/PeopleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleMVVMApp.swift
+//  PeopleMVVM
+//
+//  Created by Developer on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeopleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
