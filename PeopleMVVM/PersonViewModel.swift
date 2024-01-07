58a5f766254a11f4fda8ddc2117e24b3817b0468
erwinagpasa <rootaccess000@gmail.com>
2024-01-07 12:15:17 +0300

    adding dummy data in ViewModel

--- PeopleMVVM/PersonViewModel.swift
+++ PeopleMVVM/PersonViewModel.swift
@@ -3,6 +3,45 @@
 //  PeopleMVVM
 //
 //  Created by erwinagpasa on 07/01/2024.
-//  This just a s
+//  This just a swift file
 
-import Foundation
+import SwiftUI
+
+class PersonViewModel: ObservableObject {
+  @Published var people: [PersonModel] = []
+  
+  init(){
+    addPeople()
+  }
+  
+  func addPeople(){
+    people = peopleData
+  }
+  
+  func shuffleOrder(){
+    people.shuffle()
+  }
+  
+  func reverseOrder(){
+    people.reverse()
+  }
+  
+  func removeLastPerson(){
+    people.removeLast()
+  }
+    
+  func removeFirstPerson() {
+    people.removeFirst()
+  }
+  
+}
+
+
+//Dummy Data
+let peopleData = [
+  PersonModel(name: "Jon Snow", email: "jon@gmail.com", phoneNumber: "555-5555"),
+  PersonModel(name: "Robert Baratheon", email: "robert@gmail.com", phoneNumber: "555-5555"),
+  PersonModel(name: "Cersie Lannister", email: "cersie@gmail.com", phoneNumber: "555-5555"),
+  PersonModel(name: "Daenarys Targaryen", email: "daenays@gmail.com", phoneNumber: "555-5555"),
+  PersonModel(name: "Samwell Tarly", email: "samwell@gmail.com", phoneNumber: "555-5555")
+]
